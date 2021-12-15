8069e0638a389bbfbc4d9107b11177dbfe04cad0
Criomby <86114549+Criomby@users.noreply.github.com>
2021-12-16 00:38:15 +0100

    Update run_crypto_console.py
    Updated API response check

--- scripts/run_crypto_console.py
+++ scripts/run_crypto_console.py
@@ -12,16 +12,19 @@ def main():
 
     CGS = CoingeckoStats()
     EGS = EtherGasStats()
-    CC = CurrencyConversion()
+    CCV = CurrencyConversion()
 
-    # printing the API response is optional
-    # console.rule('[grey]API status')
-    CGS.api_call()
-    # console.print(f'CoinGecko: {CGS.api_call()}', style='grey42')
-    EGS.api_call()
-    # console.print(f'Eth Gas: {EGS.api_call()}', style='grey42')
-    # print(f'CurrencyLayer: {CC.api_call()}', style='grey42')
-    # console.rule()
+    response_cgs = check_api_status(CGS.api_call())
+    response_egs = check_api_status(EGS.api_call())
+    response_ccv = check_api_status(CCV.api_call())
+    if not (response_cgs, response_egs, response_ccv):
+        console.rule('[grey]API status')
+        console.print('[Bold red] API status review:')
+        console.print(f'CoinGecko API {response_cgs}')
+        console.print(f'ETH Gas.watch API {response_egs}')
+        console.print(f'Alpha Vantage API {response_ccv}')
+        console.input('Please fix API connection/request.\nPress any key to exit...')
+        sys.exit()
 
     console.rule('[bold red]Coin Stats')
     console.print(CGS.coins())
@@ -42,23 +45,51 @@ Market cap change 24h: {CGS.market_cap_change_percentage_24h_usd()}\n''')
 
     console.rule('[bold red]Ether Gas Stats')
     gas_stats = EGS.get_eth_gas_stats()
-    usd_eur_rate = CC.get_rate()
+    usd_eur_rate = CCV.get_fx_rate()
     gas_stats_eur = gas_stats.mul([1, usd_eur_rate], axis=0).round(decimals=2).rename(index={'usd': 'eur'})
     console.print(f'{gas_stats_eur}')
     console.rule()
 
-    console.print(f'Updated at: {get_update_time()}', style='grey42')
+    console.print(f'Updated at: {get_datetime()}', style='grey42')
+
+
+def input_diag_no_int():
+    while True:
+        usr_input = console.input('Enter "e" to exit or "t" to try again: ')
+        if usr_input == 'e':
+            sys.exit()
+        elif usr_input == 't':
+            return True
+        else:
+            continue
+
+
+def input_diag_shortcut():
+    while True:
+        usr_input = console.input('Enter "e" to exit or "r" to refresh: ')
+        if usr_input == 'e':
+            sys.exit()
+        elif usr_input == 'r':
+            return True
+        else:
+            continue
 
 
 if __name__ == '__main__':
-    try:
-        while True:
-            console.clear()
-            console.print(Panel(Align('Crypto Console v1.0', align='center'), style='navajo_white1'))
-            with console.status('Working...'):
-                main()
-            time.sleep(3600)
-    except KeyboardInterrupt:
-        pass
+    while True:
+        try:
+            while True:
+                console.clear()
+                console.print(Panel(Align('Crypto Console v1.1', align='center'), style='navajo_white1'))
+                if not check_internet_connection():
+                    console.print('[bold red]-- No internet connection. --\nConnect to the internet and try again.\n')
+                    if input_diag_no_int():
+                        continue
+                with console.status('Working...'):
+                    main()
+                time.sleep(1800)
+        except KeyboardInterrupt:
+            if input_diag_shortcut():
+                continue
 
-    # Copyright 2021 Philippe Braum
+# Copyright 2021 Philippe Braum
