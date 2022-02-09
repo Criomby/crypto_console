c08f31e5cce6bc514ae1758cba49f1ee0bbfdcf1
Criomby <86114549+Criomby@users.noreply.github.com>
2022-02-09 10:53:42 +0100

    Update to v1.2

--- scripts/crypto_ticker.py
+++ scripts/crypto_ticker.py
@@ -30,9 +30,6 @@ class CoingeckoStats:
     def coins(self):
         data = self.CG.get_coins_markets(
             vs_currency='eur',
-            # uncomment next line and define names to get desired coins
-            # ids=['bitcoin', 'ethereum', 'ripple', 'decentraland'],
-            # following line gets the defined number of coins by rank from 1 to n
             per_page=10,
             price_change_percentage='7d',
         )
@@ -47,11 +44,31 @@ class CoingeckoStats:
         }).round(decimals=2)
         df.set_index('name', inplace=True)
         df = df.rename_axis(None)
-
         data_mcf = self.market_cap_percentage()
         df['mkt_cap_%'] = data_mcf['mkt_cap_%'].values
         df = df[['symbol', 'price_€', 'rank', 'mkt_cap_%', '24h_change_%', 'curr_7d_change_%']]
         self.df_coin_prices_eur = df[['price_€']]
+        return df
+
+    def coins_11to20(self):
+        data = self.CG.get_coins_markets(
+            vs_currency='eur',
+            per_page=20,
+            price_change_percentage='7d',
+        )
+        df = pd.DataFrame.from_dict(data)
+        df = df[['name', 'symbol', 'current_price', 'market_cap_rank', 'price_change_percentage_24h',
+                 'price_change_percentage_7d_in_currency']]
+        df = df.rename(columns={
+            'current_price': 'price_€',
+            'market_cap_rank': 'rank',
+            'price_change_percentage_24h': '24h_change_%',
+            'price_change_percentage_7d_in_currency': 'curr_7d_change_%',
+        }).round(decimals=2)
+        df.set_index('name', inplace=True)
+        df = df.rename_axis(None)
+        df = df.iloc[[10, 11, 12, 13, 14]]
+        df['mkt_cap_%'] = ['na' for i in range(5)]
         return df
 
     def market_cap_change_percentage_24h_usd(self):
@@ -66,7 +83,6 @@ class CoingeckoStats:
         df = df.rename_axis(None)
         return df
 
-
     def total_market_cap(self):
         data = self.global_data['total_market_cap']['eur'] / 1000000000000
         return round(data, 2)
@@ -74,14 +90,12 @@ class CoingeckoStats:
     def trending(self):
         data = self.CG.get_search_trending()
         data = data['coins']
-
         df_data = pd.DataFrame()
         for i in range(0, len(data), 1):
             item = data[i]
             coin = item
             df = pd.DataFrame(coin.values())
-            df_data = df_data.append(df)
-
+            df_data = pd.concat([df_data, df])
         df_data_display = df_data[['symbol', 'name', 'price_btc', 'market_cap_rank']]
         df_data_display.set_index('symbol', inplace=True)
         df_data_display = df_data_display.rename_axis(None)
@@ -92,14 +106,12 @@ class EtherGasStats:
 
     def __init__(self):
         self.url = 'http://ethgas.watch/api/gas'
-
         headers = {
             'Accepts': 'application/json',
         }
 
         self.session = Session()
         self.session.headers.update(headers)
-
         self.response = dict()
 
     def api_call(self):
@@ -117,12 +129,10 @@ class EtherGasStats:
 
 class CurrencyConversion:
     def __init__(self):
-        self.url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=**your key here**'
-
+        self.url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=C4WW1E9WCPARBIHI'
         headers = {
             'Accepts': 'application/json',
         }
-
         self.session = Session()
         self.session.headers.update(headers)
 
@@ -138,7 +148,6 @@ class CurrencyConversion:
 
     def get_fx_rate(self):
         data = json.loads(self.response.text)
-        # define which currency you want to display the gas prices in (to_currency argument in url)
         self.rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
         return self.rate
 
@@ -162,5 +171,3 @@ def check_api_status(response):
         return True
     except requests.exceptions.HTTPError as e:
         return "Error: " + str(e)
-
-# Copyright 2021 Philippe Braum
